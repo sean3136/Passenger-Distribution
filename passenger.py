9b63f8dd3cc97830c7f27956860385d0f8cf6be7
Rager0321 <hp34217419@gmail.com>
2023-05-25 08:42:58 +0800

    update output message

--- passenger.py
+++ passenger.py
@@ -1,10 +1,18 @@
 from passenger_distribute import passenger_distribute
+from bus_stop import get_bus_stop
 from bus import get_bus
 from detect import detect
+bus_stops = get_bus_stop()
 bus = get_bus()
+
+print("公車站-代號: ")
+for i in bus_stops:
+  print(i.get_stop_name() + "-" + i.get_stop_id())
+print("輸入q離開")
+
 stop_id = ""
 while True:
-  stop_id = input("請輸入欲查詢站牌名稱: ")
+  stop_id = input("請輸入欲查詢站牌代號: ")
   if stop_id == "q":
     break
   else:
--- passenger_distribute.py
+++ passenger_distribute.py
@@ -11,25 +11,35 @@ def passenger_distribute(stop_id):
   bus_stop = bus_stops[int(stop_id)]
   stop_waiting = int(bus_stop.get_waiting_people())
   bus_capacity = int(bus.get_bus_capacity())
+  if bus_capacity == 0:
+    print("車輛已滿載\n")
+    return
 
+  print("「" + bus_stop.get_stop_name() + "」等待人數: " + str(stop_waiting))
+  print("到站前車輛剩餘空間: " + str(bus_capacity))
   # 等待人數小於車輛剩餘空間
   if sum_num <= bus_capacity:
     print("可上車人數為: " + str(stop_waiting) + "人")
     bus_stop.set_waiting_people(0)
     bus.set_bus_capacity(bus_capacity - stop_waiting)
+    bus_capacity = int(bus.get_bus_capacity())
+    print("到站後車輛剩餘空間: " + str(bus_capacity) + "\n")
   # 等待人數大於車輛剩餘空間
   elif sum_num > bus_capacity:
     allow_num = round(stop_waiting * bus_capacity / sum_num)
-    print("可上車人數為: " + str(allow_num) + "人")
     # 經系統分配後，車輛剩餘空間足夠
     if allow_num <= bus_capacity:
       print("經系統分配後")
       print("可上車人數為: " + str(allow_num) + "人")
       bus_stop.set_waiting_people(stop_waiting - allow_num)
       bus.set_bus_capacity(bus_capacity - allow_num)
+      bus_capacity = int(bus.get_bus_capacity())
+      print("到站後車輛剩餘空間: " + str(bus_capacity) + "\n")
     # 經系統分配後，車輛剩餘空間不足
     else: 
       print("車輛剩餘空間不足")
       print("可上車人數為: " + str(bus_capacity) + "人")
       bus_stop.set_waiting_people(stop_waiting - bus_capacity)
       bus.set_bus_capacity(0)
+      bus_capacity = int(bus.get_bus_capacity())
+      print("到站後車輛剩餘空間: " + str(bus_capacity) + "\n")
