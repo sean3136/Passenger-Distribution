08cfc153b28a5f8e99bf7875d05c9433fedfc62b
sean3136 <sean3136@gmail.com>
2023-06-07 18:11:02 +0800

    Add mainUI Add system.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import numpy as np
 import pandas as pd
 import cv as cv2
-# from google.colab.patches import cv2_imshow
 from skimage import io
 from PIL import Image
 import matplotlib.pylab as plt
+import tkinter as Tk
+import time
+import tkinter.messagebox
+import system as sys
+
+
+class Mainpage(Tk.Frame):
+    window = Tk.Tk()
+    window.title('Passenger Distribution System')
+    window.geometry('800x600')
+    window.resizable(width=False, height=False)
+    window.configure(background='white')
+
+
+if __name__ == '__main__':
+    app = Mainpage()
+    app.pack()
+    app.mainloop()
 
 
 
