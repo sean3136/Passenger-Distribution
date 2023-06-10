d206214716e822d05e820c1e88e40fddd7834605
wayne0o0 <wayne901210@gmail.com>
2023-06-11 03:27:18 +0800

    1

--- bus_stop.py
+++ bus_stop.py
@@ -28,10 +28,28 @@ class BusStop:
   def __repr__(self):
     return "站牌名稱: " + str(self.stop_name) + ", 站牌ID: " + str(self.stop_id) + ", 等車人數: " + str(self.waiting_people) + "\n"
 
-ntou_gym = BusStop("海大體育館", "0", None)
-ntou_binhai = BusStop("海大濱海校門", "1", None)
-ntou_xiangfeng = BusStop("海大祥豐校門", "2", None)
-bus_stop = [ntou_gym, ntou_binhai, ntou_xiangfeng]
-
+ntou_bus_stop = BusStop("八斗子車站", "0", None)
+ntou_bus_stop1 = BusStop("海科館", "1", None)
+ntou_bus_stop2 = BusStop("八斗子", "2", None)
+ntou_bus_stop3 = BusStop("嶺林巷", "3", None)
+ntou_bus_stop4 = BusStop("義胞新村", "4", None)
+ntou_bus_stop5 = BusStop("漁貨直銷中心", "5", None)
+ntou_bus_stop6 = BusStop("籃投溝", "6", None)
+ntou_bus_stop7 = BusStop("北寧路", "7", None)
+ntou_bus_stop8 = BusStop("海大(體育館)", "8", None)
+ntou_bus_stop9 = BusStop("海大(濱海校門)", "9", None)
+ntou_bus_stop10 = BusStop("海大(祥豐校門)", "10", None)
+ntou_bus_stop11 = BusStop("二信中學", "11", None)
+ntou_bus_stop12 = BusStop("祥豐市場", "12", None)
+ntou_bus_stop13 = BusStop("天主堂", "13", None)
+ntou_bus_stop14 = BusStop("中正區行政大樓", "14", None)
+ntou_bus_stop15 = BusStop("就業中心", "15", None)
+ntou_bus_stop16 = BusStop("信五路口", "16", None)
+ntou_bus_stop17= BusStop("市政府", "17", None)
+bus_stop = [ntou_bus_stop,ntou_bus_stop1,ntou_bus_stop2,ntou_bus_stop3,
+            ntou_bus_stop4,ntou_bus_stop5,ntou_bus_stop6,ntou_bus_stop7,
+            ntou_bus_stop8,ntou_bus_stop9,ntou_bus_stop10,ntou_bus_stop11,
+            ntou_bus_stop12,ntou_bus_stop13,ntou_bus_stop14,ntou_bus_stop15,
+            ntou_bus_stop16,ntou_bus_stop17]
 def get_bus_stop():
   return bus_stop
--- main.py
+++ main.py
@@ -1,26 +1,57 @@
 import numpy as np
 import pandas as pd
 import cv as cv2
-from skimage import io
+#from skimage import io
 from PIL import Image
 import matplotlib.pylab as plt
+from tkinter import *
 import tkinter as Tk
 import time
 import tkinter.messagebox
 import system as sys
+from passenger_distribute import passenger_distribute
+from bus_stop import get_bus_stop
+from bus import get_bus
+from detect import detect
+bus_stops = get_bus_stop()
+bus = get_bus()
 
-
+def add():
+    Label(window, text='H',bg='#E0FFFF',font=(50),fg = '#0080FF').place(x=20, y=l)
+    
+def show_busstop():
+    global l
+    l=56
+    for i in bus_stops:
+        btn = Tk.Button(window,
+                text=i.get_stop_name(),
+                font=(20),
+                relief='flat',
+                activeforeground='#0072E3',
+                justify='left',
+                width=25,
+                bg='#E0FFFF',
+                command=add()
+              )
+              
+        btn.place(x=30, y=l)
+        l=l+30;
 class Mainpage(Tk.Frame):
+    global window
     window = Tk.Tk()
     window.title('Passenger Distribution System')
     window.geometry('800x600')
     window.resizable(width=False, height=False)
-    window.configure(background='white')
-
+    window.configure(background='#E0FFFF')
+    name = Tk.StringVar("")
+    Label(window, text='search bus:',bg='#E0FFFF',font=(16)).place(x=30, y=10)
+    enter = Tk.Entry(window, textvariable=name,bd = 2,width=45,relief='sunken',show="bus_stop")
+    enter.place(x=30, y=34)
+    show_busstop()
 
 if __name__ == '__main__':
     app = Mainpage()
-    app.pack()
+    #app.pack()
     app.mainloop()
 
 
