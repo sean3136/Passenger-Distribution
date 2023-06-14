1cd58d13df9747d611ac649695d5a76ea30cae07
sean3136 <sean3136@gmail.com>
2023-06-14 22:43:36 +0800

    Modify algorithm Modify bus_stop

--- system.py
+++ system.py
@@ -5,7 +5,6 @@ import time
 
 bus_stops = get_bus_stop()
 bus = get_bus()
-sum_num = detect()
 bus_capacity = int(bus.get_bus_capacity())
 
 
@@ -20,7 +19,29 @@ def algorithm():
                       total_weight * bus_capacity)
         i.set_stop_available(avail)
         init_bus_capacity -= avail
+        print(str(i.stop_name), end="\n")
         print("各站可上車人數: " + str(avail), end="\n")
         print("各站剩餘人數: " + str(i.waiting_people - avail), end="\n")
         print("車上剩餘空位: " + str(init_bus_capacity), end="\n")
+        print("====================================", end="\n")
+
+    if (init_bus_capacity > 0):
+        max = 0
+        max_index = 0
+        for i in range(len(bus_stops)):
+            if bus_stops[i].waiting_people - bus_stops[i].stop_available >= max:
+                if bus_stops[i].stop_weight >= bus_stops[max_index].stop_weight:
+                    max = bus_stops[i].waiting_people - \
+                        bus_stops[i].stop_available
+                    max_index = i
+        print(str(bus_stops[max_index].stop_name) +
+              "可上車人數: " + str(avail), end="\n")
+        bus_stops[max_index].set_stop_available(
+            bus_stops[max_index].stop_available + init_bus_capacity)
+        init_bus_capacity = 0
+        print(str(bus_stops[max_index].stop_name) + "增加上車人數", end="\n")
+        print(str(bus_stops[max_index].stop_name) +
+              "可上車人數: " + str(bus_stops[max_index].stop_available), end="\n")
+        print("車上剩餘空位: " + str(init_bus_capacity), end="\n")
+        print("====================================", end="\n")
     time.sleep(5)
